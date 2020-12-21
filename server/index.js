83097abe02f39eccafe40ff0011ddb992cd684af
Carlos Morales <carlosjosuemn98@gmail.com>
2020-12-21 10:39:21 -0500

    update frontend url in backend

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server, {
     cors: {
-        origin: 'http://localhost:3000',
+        origin: 'https://chat-react-aplication.netlify.app',
         methods: ['GET', 'POST'],
         credentials: true,
     },
